b14f63f1d04ebb10bb9e275e3c6dd182efed22b8
CheckYourScreen <fictitiousguy@yahoo.com>
2015-10-13 15:57:13 +0530

    metalliumify

---
+++ metallium_ghost.mk
@@ -0,0 +1,26 @@
+# Check for target product
+ifeq (metallium_ghost,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := metallium_xhdpi
+
+# Inherit telephony common stuff
+$(call inherit-product, vendor/metallium/configs/telephony.mk)
+
+# Include metallium common configuration
+include vendor/metallium/main.mk
+
+$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/metallium/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/metallium/config/nfc_enhanced.mk)
+
+PRODUCT_RELEASE_NAME := MOTO X
+PRODUCT_NAME := metallium_ghost
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+endif
